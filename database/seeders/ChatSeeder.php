d2415849432ae05e00e0b7b2d3501faddd900960
ridho-code <60309031+ridho-code@users.noreply.github.com>
2025-06-10 16:13:15 +0800

    first init

---
+++ database/seeders/ChatSeeder.php
@@ -0,0 +1,341 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Chat;
+use App\Models\Message;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ChatSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $chats = [
+            [
+                'name' => 'Matematika Tutorial',
+                'subtitle' => 'Do’a Mohon Kemudahan...',
+                'time' => '09:30',
+                'badge' => '148.2K',
+                'message' => [
+                    [
+                        'content' => 'Selamat datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 1 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 2 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 3 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 1 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 2 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 3 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 1 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 2 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 3 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 1 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 2 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 3 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 1 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 2 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                    [
+                        'content' => 'Selamat 3 datang di grup Matematika Tutorial.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'SOLUSI KEUANGAN CEPAT & MUDAH',
+                'subtitle' => 'SELAMAT PAGI Yang berminat ...',
+                'time' => '08:55',
+                'badge' => '674',
+                'message' => [
+                    [
+                        'content' => 'Kami siap bantu keuangan Anda.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'Jalan Yang Jauh Jangan Lupa Pulang',
+                'subtitle' => '🎬 THUNDERBOLTS 🎬 PABRIK GUL...',
+                'time' => '08:50',
+                'badge' => '2',
+                'message' => [
+                    [
+                        'content' => 'Nonton bareng yuk!',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'The Nutcracker and the Four Realms',
+                'subtitle' => '🎬 THUNDERBOLTS 🎬 PABRIK GUL...',
+                'time' => '07:09',
+                'badge' => '8',
+                'message' => [
+                    [
+                        'content' => 'Film ini cocok ditonton keluarga.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'Proxy MTProto',
+                'subtitle' => 'Server=>Unknown Port=>443...',
+                'time' => '04:28',
+                'badge' => '14.4K',
+                'message' => [
+                    [
+                        'content' => 'Gunakan proxy berikut untuk Telegram.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'Despicable Me Sub indo',
+                'subtitle' => '🔥WONGSOBET🔥 Mem...',
+                'time' => 'Thu',
+                'badge' => '1',
+                'message' => [
+                    [
+                        'content' => 'Minion kembali beraksi!',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+            [
+                'name' => 'RESIDENT PLAYBOOK',
+                'subtitle' => 'Sudah tayang ya',
+                'time' => 'Thu',
+                'badge' => '',
+                'message' => [
+                    [
+                        'content' => 'Jangan lupa nonton episodenya.',
+                        'user_id' => 2
+                    ],
+                ]
+            ],
+        ];
+        foreach ($chats as $chat) {
+            $c = Chat::create([
+                'name' => $chat['name'],
+                'subtitle' => $chat['subtitle'],
+                'time' => $chat['time'],
+                'badge' => $chat['badge'],
+                'user_id' => 1
+            ]);
+            foreach ($chat['message'] as $message) {
+                Message::create([
+                    'content' => $message['content'],
+                    'user_id' => $message['user_id'],
+                    'chat_id' => $c->id
+                ]);
+            }
+        };
+    }
+}
